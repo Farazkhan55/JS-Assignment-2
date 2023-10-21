74968f82a48ce8a167d7f07db1c7e3c3ad018341
Farazkhan55 <fk417145@gmail.com>
2023-10-21 23:42:23 +0500

    javascript-assignment

---
+++ app.js
@@ -0,0 +1,56 @@
+// CHAPTER 1
+// QUESTION 1
+
+
+// var num = (4)
+
+// // num1 = num * 1
+// // console.log(num1)
+
+// var num = (4)
+// num2 = num * 2
+// console.log(num2)
+
+// num3 = num * 3
+// console.log(num3)
+
+
+
+// num4 = num * 4
+// console.log(num4)
+
+
+// num5 = num * 5
+// console.log(num5)
+
+// num6 = num * 6
+// console.log(num6)
+
+
+// num7 = num * 7
+// console.log(num7)
+
+
+// num8 = num * 8
+// console.log(num8)
+
+
+// num9 = num * 9
+// console.log(num9)
+
+
+// num10 = num * 10
+// console.log(num10)
+
+
+
+
+// NESTETD (- +)
+
+
+
+
+// var num = ((5 + 3) + 4)
+// console.log(num)
+
+
